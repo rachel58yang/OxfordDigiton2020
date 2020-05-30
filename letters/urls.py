01263192bbfb09bc41002532b2958b376f69787e
Katie House <katiehouse3@gmail.com>
2020-05-30 17:57:25 -0400

    help me matt

--- letters/urls.py
+++ letters/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
     path('addLetter', views.add_letter, name='add_letter'),
     path('letters', views.view_letters, name='view_letters'),
     path('response', views.response, name='response')
--- letters/views.py
+++ letters/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'letters/index.html', {})
 
+def login(request):
+    return render(request, 'letters/login.html', {})
+
 def add_letter(request):
     return render(request, 'letters/add_letter.html', {})
 
