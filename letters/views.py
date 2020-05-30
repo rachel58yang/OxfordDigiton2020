f063b2a919ee52338faea2cc5f9aea8784adc446
mjr68 <matthewrose1799@gmail.com>
2020-05-30 19:09:22 -0400

    added pseudo random letter generation in /letters page

--- letters/views.py
+++ letters/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
+from random import random
 
 
 def home(request):
@@ -11,7 +13,10 @@ def add_letter(request):
     return render(request, 'letters/add_letter.html', {})
 
 def view_letters(request):
-    return render(request, 'letters/view_letters.html', {})
+    number_of_records = Post.objects.count()
+    random_index = int(random()*number_of_records)+1
+    random_letter = Post.objects.get(pk = random_index)
+    return render(request, 'letters/view_letters.html', {'letter': random_letter})
 
 def response(request):
     return render(request, 'letters/response.html', {})
