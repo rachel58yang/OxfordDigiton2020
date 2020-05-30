5f9701ea2977ac1712451bcd9bd3501f7b741a99
Rachel <rachel58yang@gmail.com>
2020-05-30 19:58:11 -0400

    merge conflict

--- letters/views.py
+++ letters/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import PostForm
 
 
 def home(request):
@@ -8,6 +9,17 @@ def login(request):
     return render(request, 'letters/login.html', {})
 
 def add_letter(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.create_date = timezone.now()
+            post.response = 0;
+            post.save()
+            return redirect('letters/add_letter.html', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'letters/add_letter.html', {})
 
 def view_letters(request):
