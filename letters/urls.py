868a35a0130393cab6d6e6c8c8f0fedbc5fd8055
mjr68 <matthewrose1799@gmail.com>
2020-05-30 23:30:05 -0400

    updated nav bar and removed buttons

--- letters/urls.py
+++ letters/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('home', views.home, name='home'),
     path('addLetter', views.add_letter, name='add_letter'),
-    path('addLetter/new', views.post_new, name='post_new'),
+    # path('addLetter/new', views.post_new, name='post_new'),
     path('letters', views.view_letters, name='view_letters'),
-    path('response/<int:id>/', views.response, name='response')
+    path('response/<int:id>/', views.response, name='response'),
+    path('account', views.account_info, name='account_info')
 ]
--- letters/views.py
+++ letters/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
 from random import random
-
+from django.utils import timezone
 
 def login(request):
     return render(request, 'letters/index.html', {})
@@ -11,22 +11,20 @@ def home(request):
     return render(request, 'letters/home.html', {})
 
 def add_letter(request):
-    return render(request, 'letters/add_letter.html', {})
-
-def post_new(request):
     if request.method == "POST":
-            form = PostForm(request.POST)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.create_date = timezone.now()
-                post.text = request.POST.get('text')
-                post.response = 0
-                post.save()
-                return render('letters/view_letter.html')
-            else:
-                form = PostForm()
-                return render(request, 'letters/add_letter.html', {})
+        form = PostForm(request.POST)
+        # print(form)
+        # print("________")
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.create_date = timezone.now()
+            post.response = 0
+            post.save()
+            return redirect('letters/add_letter.html', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'letters/add_letter.html', {})
 
 def view_letters(request):
     # filter out responses
@@ -39,4 +37,11 @@ def view_letters(request):
 def response(request, id):
     letter = Post.objects.get(pk = id)
     return render(request, 'letters/response.html', {'letter': letter})
+
+def account_info(request):
+    user = None
+    print(request.user)
+    #if request.user.is_authenticated():
+    #    user = request.user
+    return render(request, 'letters/account_info.html', {'user': user})
 
