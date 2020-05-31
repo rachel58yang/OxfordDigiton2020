a93ce6c8547a2befc59083148b9144778a001041
Rachel <rachel58yang@gmail.com>
2020-05-31 08:29:11 -0400

    merge conflict

--- letters/urls.py
+++ letters/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('home', views.home, name='home'),
     path('addLetter', views.add_letter, name='add_letter'),
-    path('addLetter/new', views.post_new, name='post_new'),
     path('letters', views.view_letters, name='view_letters'),
     path('response/<int:id>/', views.response, name='response')
 ]
